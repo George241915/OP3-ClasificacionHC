70a1837a789eb7dd55fb02797a502dc4b9dc74e0
George Erazo <george241915@gmail.com>
2023-08-06 01:24:09 -0500

    Style Navigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,55 +1,75 @@
-import { View, Text ,Image} from 'react-native'
-import React from 'react'
+import { View, Image, Text} from 'react-native'
+import React, { useRef } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Navegacion from '../screens/Navegacion';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeUser from '../screens/HomeUser';
 import Statistics from '../screens/Statistics';
 import Classification from '../screens/Classification';
-import Icon  from "react-native-vector-icons/FontAwesome5";
+import { FontAwesome5 } from '@expo/vector-icons';
+import styles from '../styles/stylesNavegacion';
 
+//import camicon
+import cam from '../assets/logEC.png'
+import { Vibration } from 'react-native';
 
 const Tab= createBottomTabNavigator();
 
 export default function Navigation() {
+ 
   return (
-    <Tab.Navigator>
-      
-      <Tab.Screen name='User' component={HomeUser} 
+      <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarActiveTintColor: '#efb810',
+      tabBarInactiveTintColor: 'black',
+      tabBarStyle: [styles.tabnav],
+      tabBarIcon: ({ color }) => screenOptions(route, color),
+      tabBarLabel: ({focused, color }) => {
+        let labelicon = '';
+        switch (route.name) {
+          case 'Perfil':
+            labelicon = focused ? 'Perfil' : '';
+            break;
+          case 'Classification':
+            labelicon = focused ? '' : '';
+            break;
+          case 'Estadisticas':
+            labelicon = focused ? 'Estadisticas' : '';
+            break; 
+        } 
+        return <Text style={{color:'purple'}}>{labelicon}</Text>
+      }
+    })}>
+      <Tab.Screen name='Perfil' component={HomeUser}
         options={{
-          tabBarIcon:({color,size})=>{
-            <Icon name='user' color={color}size={size}></Icon>
-          },
-          headerShown:false
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.tabvaricon}>
+              <FontAwesome5 name="user" size={20} color={focused ? 'purple' : 'gray'}>
+              </FontAwesome5>
+            </View>),
+            headerShown: false,
         }}
       ></Tab.Screen>
       <Tab.Screen name='Classification' component={Classification}
         options={{
-          tabBarLabel:"",
-          tabBarIcon:()=>renderEggs(),
-          headerShown:false,
-          
+          tabBarIcon: ({ focused }) => (
+            
+              <View style={styles.actionboton} >
+                <Image source={cam} style={styles.camicon}>
+                </Image>
+              </View>
+            
+          ) , 
+          headerShown: false
         }}
       ></Tab.Screen>
-      <Tab.Screen name='Statistics' component={Statistics}
+      <Tab.Screen name='Estadisticas' component={Statistics}
         options={{
-          tabBarIcon:({color,size})=>{
-            <Icon name='' color={color}size={size}></Icon>
-          },
-          headerShown:false
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.tabvaricon}>
+              <FontAwesome5 name="chart-line" size={20} color={focused ? 'purple' : 'gray'}>
+              </FontAwesome5>
+            </View>),
+          headerShown: false
         }}
       ></Tab.Screen>
-      
     </Tab.Navigator>
-    
-  )
-}
-
-function renderEggs(){
-  return(
-    <Image
-      source={require("../assets/logEC.png")}
-      style={{width: 55, height:55}}
-    ></Image>
-  )
+    )
 }
